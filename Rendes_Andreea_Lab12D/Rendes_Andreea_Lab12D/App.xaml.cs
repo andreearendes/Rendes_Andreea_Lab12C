221f3ce4043dceb556979ef3ffc0d79bdb62384b
Andreea Rendes <andreearendes@outlook.com>
2022-01-08 15:17:09 +0200

    Add project files.

---
+++ Rendes_Andreea_Lab12D/Rendes_Andreea_Lab12D/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rendes_Andreea_Lab12D.Data;
+
+
+namespace Rendes_Andreea_Lab12D
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Rendes_Andreea_Lab12D/Rendes_Andreea_Lab12D/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Rendes_Andreea_Lab12D.Models;
+
+namespace Rendes_Andreea_Lab12D.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
